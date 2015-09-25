1e6ff0025e8ce4ecf56284757d8483f311485f54
muhammadulilalbab <muhammadulilalbab91@gmail.com>
2015-09-25 09:20:13 +0700

    improve substring.c

--- substring.c
+++ substring.c
@@ -1,20 +1,38 @@
+/*Program untuk menghitung kemunculan substring
+ * 
+ * */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 int main()
 {
+	/* kamus */
 	char * S1;
 	char * S2;
-	
-	S1 = (char *) malloc (sizeof(char)*2);
-	S2 = (char *) malloc (sizeof(char)*2);
 	int i, s1Length,s2Length,count=0;
+	
+	/*Algoritma */
+	/* alokasi memori string */
+	S1 = (char *) malloc (sizeof(char)*100);
+	S2 = (char *) malloc (sizeof(char)*100);
+	
+	/* baca string dan substringnya*/
 	scanf("%s",S1);
 	scanf("%s",S2);
-	S1[strlen(S1)-1]=0;
-	S2[strlen(S2)-1]=0;
+	
+	/*menghilangkan karakter terakhir string dan substring jika karakter terakhir adalah . */
+	if(S1 && *S1 && S1[strlen(S1) - 1] == '.')
+	{
+		S1[strlen(S1)-1]=0;
+	}
+	if(S2 && *S2 && S2[strlen(S2) - 1] == '.')
+	{
+		S2[strlen(S2)-1]=0;
+	}
 	s1Length = strlen(S1);
 	s2Length = strlen(S2);
+	
+	/*tampilkan string dan substring yang dibaca */
 	for(i=0;i<s1Length;i++)
 	{
 		
@@ -27,6 +45,7 @@ int main()
 	}
 	printf("\n");
 	
+	/* hitung jumlah kemunculan substring */
 	while(S1 = strstr(S1, S2))
 	{
 		count++;
@@ -36,3 +55,4 @@ int main()
 	printf("munculnya sebanyak %d\n",count);
 	return 0;
 }
+
